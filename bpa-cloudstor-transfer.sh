30dce020c0521a3a837f02df6f851b61a1f6b318
Mark Tearle <mark.tearle@qcif.edu.au>
2020-11-27 10:52:36 +0800

    Initial pseudocode of script

---
+++ bpa-cloudstor-transfer.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# bpa-cloudstor-transfer.sh
+
+# Usage: bpa-cloudstor-transfer.sh <folder-to-transfer>
+
+# Initial pseudocode for script
+
+# Need the following configuration information
+# - User details
+# - App password
+
+# - Data age
+# - Destination to share to (ie BPA CloudStor address)
+# - Notification email address (help@bioplatforms.com)
+
+# Check we've got config information
+
+# Check we've got rclone installed
+# Check we've got mail installed
+
+# (Re) generate rclone config
+
+# Check number of arguments
+
+# Check argument is directory
+
+# Check directory is named correctly
+
+# Test if folder is present on CloudStor
+
+# Test if we've got enough space on CloudStor
+
+# Rclone to folder on CloudStor
+
+# Use owncloud API to share to BPA CloudStor address
+
+# Generate email to notification email address
+
+# Report to user
